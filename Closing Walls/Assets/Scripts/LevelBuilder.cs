8e9a013a748af29d7cae1c0b594e3e0783b154a0
Sardet <partycrasherer@gmail.com>
2023-09-30 10:48:37 -0700

    levelbuilder boilerplate

---
+++ Closing Walls/Assets/Scripts/LevelBuilder.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class LevelBuilder : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+} 
