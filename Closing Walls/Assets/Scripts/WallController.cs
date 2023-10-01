57f7fccb584d397371dada27d2b3b19f70c864d6
BericMoeller <bericmoeller@gmail.com>
2023-09-30 17:47:26 -0700

    Made Some Sprites!

---
+++ Closing Walls/Assets/Scripts/WallController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Wall1;
+    public GameObject Wall2;
+    public int width;
+    public int secondsLeft;
+    private double endTime;
+    public double startTime;
+    private float speedToClose;
+    void Start()
+    {
+        startTime = Time.timeAsDouble + 0;
+        speedToClose = (width / 2) / secondsLeft;
+        endTime = startTime + secondsLeft;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        secondsLeft = (int)(endTime - Time.timeAsDouble);
+        Wall1.transform.position = new Vector3(0 - (speedToClose * secondsLeft), 0, 0);
+        Wall2.transform.position = new Vector3(0 + (speedToClose * secondsLeft), 0, 0);
+    }
+}
