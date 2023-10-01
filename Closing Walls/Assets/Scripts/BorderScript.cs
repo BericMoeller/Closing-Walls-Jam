eade1336590c11083e3a92d1795022c18331241a
Sardet <partycrasherer@gmail.com>
2023-10-01 00:56:07 -0700

    BLACK BORDERS WORKING!?!!?!?!?!

--- Closing Walls/Assets/Scripts/BorderScript.cs
+++ Closing Walls/Assets/Scripts/BorderScript.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class BorderScript : MonoBehaviour
 {
+    private Vector3 growthy;
+    private Vector3 moveleft;
+    private Vector3 moveright;
+
+    [SerializeField]
+    private Transform left;
+    
+    [SerializeField]
+    private Transform right;
+    
+    [SerializeField]
+    private Transform top;
+
+    [SerializeField]
+    private Transform bottom;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+    growthy = new Vector3(0f, 0.01f, 0f);
+    moveleft = new Vector3(-0.01f, 0f, 0f);
+    moveright = new Vector3(0.01f, 0f, 0f);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        bottom.localScale += growthy;
+        top.localScale += growthy;
+        left.localPosition += moveright;
+        right.localPosition += moveleft;
     }
 }
